810c7249de3debe1e0fad38eb56093b11805f5f8
Jokerinya <sakacibrahim@gmail.com>
2020-10-17 01:19:46 +0300

    index page working

--- mysql-app.py
+++ mysql-app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+developer = "Jokerinya"
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -38,6 +39,25 @@ cursor.execute(drop_table)
 cursor.execute(phonebook_table)
 cursor.execute(data)
 
+def find_person(keyword):
+    query=f"SELECT person, number FROM phonebook WHERE person like '%{keyword}%';"
+    cursor.execute(query)
+    raw_result = cursor.fetchall()  # tuple type
+    result = [{"name" : row[0], "number": row[1]} for row in raw_result]
+    if not any(result):
+        result = [{"name" : "Not Found", "number": "Not Found"}]
+    return result
+
+# Routings
+# index page
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method=="POST":
+        keyword = request.form["username"].strip().title()
+        persons = find_person(keyword)
+        return render_template("index.html", developer_name=developer, show_result=True ,no_result=False, keyword=keyword, persons=persons)
+    else:
+        return render_template("index.html", developer_name=developer, show_result=False)
 
 
 
